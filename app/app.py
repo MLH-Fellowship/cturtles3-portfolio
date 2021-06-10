fcc06eb35d5cda0fc55d22d5a9dc868bed383c89
ccejudo <cristopher.cm1999@gmail.com>
2021-06-10 15:14:59 -0500

    Retrieve information from json completed

--- app/app.py
+++ app/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, url_for, request, redirect
 import os
+import json
 
 app = Flask(__name__)
+data_file = open('./static/data.json')
+data = json.load(data_file)
+data_file.close()
 
 #Create URL routes
 @app.route('/')
@@ -15,7 +19,8 @@ def home():
 #Create URL for each members
 @app.route('/about/<string:name>')
 def about(name):
-    return render_template("about.html", name=name)
+    userData = data[name]
+    return render_template("about.html", name=name, userData=userData)
 
 
 if __name__ == "__main__":
