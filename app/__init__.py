482a1512afe4bc9dcdd1218269f20ab1e314cdcc
Yenyu Huang AWS <yenyu.huang99@gmail.com>
2021-06-30 13:44:37 +0000

    register's inconsistent spacings

--- app/__init__.py
+++ app/__init__.py
@@ -34,17 +34,17 @@ def register():
         elif not password:
             error = 'Password is required.'
         elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
+	    'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
-            error = f"User {username} is already registered."
+            error = "User {username} is already registered."
 
         if error is None:
             db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
+	        'INSERT INTO user (username, password) VALUES (?, ?)',
                 (username, generate_password_hash(password))
             )
             db.commit()
-            return f"User {username} created successfully"
+            return f"User {username} created successfully\n"
         else:
             return error, 418
 
@@ -59,21 +59,22 @@ def login():
         password = request.form.get('password')
         db = get_db()
         error = None
-		user = db.execute(
-		'SELECT * FROM user WHERE username = ?', (username,)).fetchone()
-		
-		if user is None:
-			error = 'Incorrect username.'
-		elif not check_password_hash(user['password'], password):
-			error = 'Incorrect password.'
+	user = db.execute(
+	    'SELECT * FROM user WHERE username = ?', (username,)
+	).fetchone()
+			
+	if user is None:
+	    error = 'Incorrect username.'
+	elif not check_password_hash(user['password'], password):
+	    error = 'Incorrect password.'
 
-		if error is None:
-			return "Login Successful", 200
-		else:
-			return error, 418
+	if error is None:
+	    return "Login Successful\n", 200
+	else:
+	    return error, 418
 	
-	## TODO: Return a login page
-	return "Login Page not yet implemented", 501    
+    ## TODO: Return a login page
+    return "Login Page not yet implemented", 501    
 
 @app.route('/health')
 def starting_url():
